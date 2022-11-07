47ccbe41d5d3d1b4ca06d9098a7d30ec2dd3fe46
hjpotter92 <git@hjpotter92.email>
2022-11-07 11:19:04 +0530

    Add editorconfig file for sh, py and go file
    Add log statements to script

--- start.sh
+++ start.sh
@@ -3,13 +3,19 @@
 SCRIPT_PATH=$(realpath $(dirname $0))
 cd "$SCRIPT_PATH"
 
-if [ -d ".venv" ]
-then
-    . .venv/bin/activate
+echo "Starting UPS metric update script"
+
+if [ -d ".venv" ]; then
+  source .venv/bin/activate
+  echo "Activated virtual env"
 else
-    python3 -m venv .venv
-    . .venv/bin/activate
-    pip install -U requirements.txt
+  python3 -m venv .venv
+  echo "Created new virtual env"
+  source .venv/bin/activate
+  pip install -U requirements.txt
+  echo "Update venv with required packages"
 fi
 
+echo "Running script now"
 python3 script.py
+echo "Script finished"
