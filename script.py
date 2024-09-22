9ce016db0895a45d974f735130d776476345e0c4
hjpotter92 <hjpotter92+github@gmail.com>
2024-09-22 21:08:45 +0530

    script.py: Use official PyNUTClient library for connecting to UPS

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from os import getenv
-from nut2 import PyNUTClient
+from PyNUTClient.PyNUT import PyNUTClient
 from thingspeak import Channel
 
 CHANNEL_ID = int(getenv("THINGSPEAK_CHANNEL_ID"))
@@ -8,5 +8,10 @@ UPS_NAME = getenv("NUT_UPS_NAME")
 
 channel = Channel(CHANNEL_ID, api_key=API_KEY)
 client = PyNUTClient()
-voltage = float(client.get_var(UPS_NAME, 'input.voltage'))
-print(voltage, channel.update({"field1": voltage}))
+if client.CheckUPSAvailable(UPS_NAME):
+    data = client.GetUPSVars(UPS_NAME)
+    # use byte sequence instead of str as suggested by docs
+    voltage = float(data[b'input.voltage'])
+    print(voltage, channel.update({"field1": voltage}))
+else:
+    raise Exception("Failed to verify if ups was online")
